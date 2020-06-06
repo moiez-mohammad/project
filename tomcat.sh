c983e4cd5794f3c358a0e8b4d4dc1c3031d1f178
Ubuntu <ubuntu@ip-172-31-46-60.ap-south-1.compute.internal>
2020-06-06 18:20:15 +0000

    installing tomcat

--- tomcat.sh
+++ tomcat.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.50/bin/apache-tomcat-8.5.50.tar.gz
-tar -xvzf apache-tomcat-8.5.50.tar.gz
-mv apache-tomcat-8.5.50.tar.gz tomcat8
-rm -rf apache-tomcat-8.5.50.tar.gz
+echo "Installing tomcat"
 
+wget https://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.55/bin/apache-tomcat-8.5.55.tar.gz
+tar -xvzf apache-tomcat-8.5.55.tar.gz
+mv apache-tomcat-8.5.55.tar.gz tomcat8
+rm -rf apache-tomcat-8.5.50.tar.gz
